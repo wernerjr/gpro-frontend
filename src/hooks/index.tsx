4c2605e3bafd223769767fd387774fdf79850565
Werner gehrke Junior <werner.junior@hbsis.com.br>
2020-08-17 22:30:11 -0300

    Added header and menu components

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import { AuthProvider } from './auth';
+import Header from '../components/Header';
+import Menu from '../components/Menu';
 
 const AppProvider: React.FC = ({ children }) => (
-  <AuthProvider>{children}</AuthProvider>
+  <>
+    <Header />
+    <Menu />
+    <AuthProvider>{children}</AuthProvider>
+  </>
 );
 
 export default AppProvider;
